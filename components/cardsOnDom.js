f1a7bec438ec0e94ac603818c062cc1ec01f567c
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-04-30 13:55:02 -0500

    added logic

---
+++ components/cardsOnDom.js
@@ -0,0 +1,13 @@
+import renderToDom from "../Utils/renderToDom.js";
+
+const cardsOnDom = (array) => {
+  let domString = "";
+  array.forEach((studentObj) => {
+
+    domString += ""
+   
+  })
+renderToDom("card-container", domString)
+};
+
+export default cardsOnDom
--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,57 @@
-import renderToDom from "../Utils/renderToDom.js";
-
-
-
-
-
-// add form function
-const addForm = () => {
-  const domString = `<div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Please enter your name!</label>
-  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="First name, Last name">
-</div>`;
-  renderToDom('#enter-student-form', domString);
-}
-
-
-
-
-//add filter buttons
-
-
-
-//add sorting hat function
-
-
+import renderToDom from "../Utils/renderToDom.js"
+import addForm from "/components/addForm.js"
+import addButtons from "/components/addButtons.js"
+import students from "../Data/Students.js"
+import cardsOnDom from "../components/cardsOnDom.js"
 
 //cardsOnDom function
 
 
-
-
 //event listeners
+const eventListeners = () => {
+//sortingHat    
+  const form = document.querySelector("#enter-student-form");
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-    //filterbuttons
-    //expel student function
+    const houses = ['Ravenclaw', 'Gryffindor', 'Slytherin', 'Hufflepuff'];
+    
+    const randomHouse = houses[Math.floor(Math.random() * houses.length)];
+
+    let houseColor = ""
+    if (randomHouse === houses[0]) {
+      houseColor = 'blue'
+    } else if (randomHouse === houses[1]) {
+      houseColor = 'gold'
+    } else if (randomHouse === houses[2]) {
+      houseColor = 'green'
+    } else {
+      houseColor = 'yellow'
+    }
+
+    const newStudent = {
+      student_id: students.length,
+      name: document.querySelector('#name').value,
+      color: houseColor,
+      house: randomHouse,
+      expelled: false
+
+    };
+
+    students.push(newStudent);
+    console.log(students);
+    form.reset();
+
+  });
+//filterbuttons
+
+//expel student function
+
+}
 
 
 
     addForm()
+    addButtons()
+    cardsOnDom()
+    eventListeners()
