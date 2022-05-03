c04bc218514fc3da200f7be3c6e081087ce1021d
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-05-03 18:09:28 -0500

    style cards

--- components/addForm.js
+++ components/addForm.js
@@ -8,7 +8,7 @@ const addForm = () => {
     <label for="first-name-last-name" class="form-label">Enter your name!</label>
     <input type="name" class="form-control" id="student-name">
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button id="submit-btn" type="submit" class="btn btn-primary">Submit</button>
 </form>`;
   renderToDom('#enter-student-form', domString);
 }
